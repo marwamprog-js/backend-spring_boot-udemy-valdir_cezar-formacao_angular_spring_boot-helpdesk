b7aa204dcc8488f5981c115bf5f8cca155815eb1
marwamprog-js <marwamprog.js@gmail.com>
2023-10-19 10:41:16 -0300

    Validação nos campos de criação de novo usuário

--- src/main/java/com/maltadev/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/maltadev/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,9 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.maltadev.helpdesk.domain.Tecnico;
 import com.maltadev.helpdesk.domain.enums.Perfil;
@@ -15,10 +18,23 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Long id;
+	
+	@NotNull(message = "O campo Nome é requerido")
+	@NotBlank(message = "O campo Nome é requerido")
 	protected String nome;
+	
+	@NotNull(message = "O campo CPF é requerido")
+	@NotBlank(message = "O campo CPF é requerido")
 	protected String cpf;
+	
+	@NotNull(message = "O campo Email é requerido")
+	@NotBlank(message = "O campo Email é requerido")
 	protected String email;
+	
+	@NotNull(message = "O campo Senha é requerido")
+	@NotBlank(message = "O campo Senha é requerido")
 	protected String senha;
+	
 	protected Set<Integer> perfis = new HashSet<>();
 	
 	@JsonFormat(pattern = "dd/MM/yyyy")
